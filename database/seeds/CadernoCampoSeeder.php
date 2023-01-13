568f8c77246a428fd4a60b2f375ee65da5e23fb6
Alan Tygel <alantygel@gmail.com>
2023-01-13 14:25:32 -0300

    Nova estrutura do caderno de campo/ficha de visita

--- database/seeds/CadernoCampoSeeder.php
+++ database/seeds/CadernoCampoSeeder.php
@@ -23,11 +23,13 @@ class CadernoCampoSeeder extends Seeder
 
         \App\Models\Core\TemplatePerguntaModel::insert([
             ['id' => 1, 'pergunta' => 'Data da Visita', 'tipo' => 'date', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
-            ['id' => 2, 'pergunta' => 'Horário de Início', 'tipo' => 'time', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
-            ['id' => 3, 'pergunta' => 'Horário de Fim', 'tipo' => 'time', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
-            ['id' => 4, 'pergunta' => 'Cultivos Existentes', 'tipo' => 'text', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
-            ['id' => 5, 'pergunta' => 'Observações', 'tipo' => 'text', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
-            ['id' => 6, 'pergunta' => 'Notas', 'tipo' => 'text', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
+            ['id' => 2, 'pergunta' => 'Horário de Saída da ETEC', 'tipo' => 'time', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
+            ['id' => 3, 'pergunta' => 'Horário de Início da Visita', 'tipo' => 'time', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
+            ['id' => 4, 'pergunta' => 'Horário de Fim da Visita', 'tipo' => 'time', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
+            ['id' => 5, 'pergunta' => 'Horário de chegada na ETEC', 'tipo' => 'time', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
+            ['id' => 6, 'pergunta' => 'Descrição das atividades', 'tipo' => 'text', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
+            ['id' => 7, 'pergunta' => 'Orientações técnicas', 'tipo' => 'text', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
+            ['id' => 8, 'pergunta' => 'Notas', 'tipo' => 'text', 'tags' => '', 'created_at' => $createdAt, 'updated_at' => $createdAt],
         ]);
 
         \App\Models\Core\TemplatePerguntaTemplatesModel::insert([
@@ -37,6 +39,8 @@ class CadernoCampoSeeder extends Seeder
             ['template_pergunta_id' => 4, 'template_id' => 1, 'ordem' => '4', 'created_at' => $createdAt, 'updated_at' => $createdAt],
             ['template_pergunta_id' => 5, 'template_id' => 1, 'ordem' => '5', 'created_at' => $createdAt, 'updated_at' => $createdAt],
             ['template_pergunta_id' => 6, 'template_id' => 1, 'ordem' => '6', 'created_at' => $createdAt, 'updated_at' => $createdAt],
+            ['template_pergunta_id' => 7, 'template_id' => 1, 'ordem' => '7', 'created_at' => $createdAt, 'updated_at' => $createdAt],
+            ['template_pergunta_id' => 8, 'template_id' => 1, 'ordem' => '8', 'created_at' => $createdAt, 'updated_at' => $createdAt],
         ]);
     }
 }
